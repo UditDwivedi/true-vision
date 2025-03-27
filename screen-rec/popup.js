4e2887fb37138efccc0408ad6b69373ba6be4002
UditDwivedi <udit.dwivedi.03@gmail.com>
2025-03-27 22:42:03 +0530

    lld

--- screen-rec/popup.js
+++
@@ -1,7 +0,0 @@
-const startRecording = () => {
-    chrome.runtime.sendMessage({ name: 'startRecording' });
-  };
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('startRecordingButton').addEventListener('click', startRecording);
-  });
